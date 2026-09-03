3c346bbf9ec85b4506c704cd35cc42240b672077
YUKIF69 <159157261+YUKIF69@users.noreply.github.com>
2026-09-03 16:39:34 +0300

    fix: prisma client generation path

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -3,7 +3,7 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { PrismaService } from '../../prisma/prisma.service';
 import { NotificationsService } from '../notifications/notifications.service';
 import { PingGateway } from '../gateway/ping.gateway';
-import { Monitor } from '@prisma/client';
+import { Monitor } from '../../generated/prisma';
 
 @Injectable()
 export class SchedulerService {
